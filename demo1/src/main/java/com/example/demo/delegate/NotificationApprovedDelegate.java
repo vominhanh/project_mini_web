4f700d2e812a731222b749c0f0d850cfc207b3b2
MinhAnh <avminh824@gmail.com>
2026-04-17 11:20:31 +0700

    feat: Implement notification delegates for approved and rejected room bookings, update BPMN process, and enhance UI components

--- demo1/src/main/java/com/example/demo/delegate/NotificationApprovedDelegate.java
+++ demo1/src/main/java/com/example/demo/delegate/NotificationApprovedDelegate.java
@@ -1,4 +1,21 @@
 package com.example.demo.delegate;
 
-public class NotificationApprovedDelefate {
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component("notificationApproved")
+public class NotificationApprovedDelegate  implements JavaDelegate {
+
+    private static final Logger log = LoggerFactory.getLogger(NotificationApprovedDelegate.class);
+
+    @Override
+    public void execute(DelegateExecution execution) {
+        Object approved = execution.getVariable("approved");
+        Object roomId = execution.getVariable("roomId");
+        Object roomName = execution.getVariable("roomName");
+        log.info("Room workflow audited: roomId={}, roomName={}, approved={}", roomId, roomName, approved);
+    }
 }
--- demo1/src/main/java/com/example/demo/delegate/NotificationRejectDelegate.java
+++ demo1/src/main/java/com/example/demo/delegate/NotificationRejectDelegate.java
@@ -6,9 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-@Component("auditLog")
-public class AuditLogDelegate implements JavaDelegate {
-    private static final Logger log = LoggerFactory.getLogger(AuditLogDelegate.class);
+@Component("notificationReject")
+public class NotificationRejectDelegate implements JavaDelegate {
+    private static final Logger log = LoggerFactory.getLogger(NotificationRejectDelegate.class);
 
     @Override
     public void execute(DelegateExecution execution) {
